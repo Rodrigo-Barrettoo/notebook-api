831b54742de1bcb158c59d672ab89cbd3cb1ade7
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-22 09:39:59 -0300

    Usando POST com associações (belongs_to)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,26 +1,26 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def author
-    "Barretin"
-  end
+  # def author
+  #   "Barretin"
+  # end
 
-  # criando um metodo o json nao fica encadeado
-  # com o include ele fica encadeado
-  def kind_description
-    self.kind.description
-  end
+  # # criando um metodo o json nao fica encadeado
+  # # com o include ele fica encadeado
+  # def kind_description
+  #   self.kind.description
+  # end
 
-  def as_json(options = {})
-    super(
-      root: true,
-      methods: [:kind_description, :author],
+  # def as_json(options = {})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author],
 
-      include: {
-        kind: {
-          only: [:description, :created_at]
-        }
-      }
-    )
-  end
+  #     include: {
+  #       kind: {
+  #         only: [:description, :created_at]
+  #       }
+  #     }
+  #   )
+  # end
 end
