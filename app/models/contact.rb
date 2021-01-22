903f1a7a0f7adcce00d006486d824988c0336649
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-22 09:21:46 -0300

    render json com associações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,22 @@ class Contact < ApplicationRecord
     "Barretin"
   end
 
+  # criando um metodo o json nao fica encadeado
+  # com o include ele fica encadeado
+  def kind_description
+    self.kind.description
+  end
+
   def as_json(options = {})
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_description, :author],
+
+      include: {
+        kind: {
+          only: [:description, :created_at]
+        }
+      }
+    )
   end
 end
