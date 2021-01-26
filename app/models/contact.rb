017bb509af00d3aeb46a6ae748db10f87e157570
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-26 15:29:03 -0300

    I18n para data (as_json)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,20 +4,26 @@ class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_many :phones
 
-  def birthdate_br
-    I18n.l(self.birthdate) unless self.birthdate.blank?
+  def as_json(options = {})
+    hash = super(options)
+    hash[:birthdate] = I18n.l(self.birthdate) unless self.birthdate.blank?
+    hash
   end
 
-  def to_br
-    {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
-      created_at: self.created_at,
-      updated_at: self.updated_at,
-      kind_id: self.kind_id,
-    }
-  end
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+  #     created_at: self.created_at,
+  #     updated_at: self.updated_at,
+  #     kind_id: self.kind_id,
+  #   }
+  # end
 
   # def author
   #   "Barretin"
