adba4d97c45ade102feccc2967c50b577f21f82f
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-26 16:03:38 -0300

    CRUD com nested attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,7 @@ class Contact < ApplicationRecord
   # Associates
   belongs_to :kind, optional: true
   has_many :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def as_json(options = {})
     hash = super(options)
