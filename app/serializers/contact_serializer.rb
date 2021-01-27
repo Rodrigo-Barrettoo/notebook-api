97bb79af4640a14d767be2c8685eebe68f718bcc
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-27 14:01:52 -0300

    associações

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind, optional: true
+  has_many :phones
+  has_one :address
+
   def attributes(*args)
     hash = super(*args)
     # pt-BR => hash[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
