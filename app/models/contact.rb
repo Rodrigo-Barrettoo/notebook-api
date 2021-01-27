0bc2b1d00e6a859cd167741ea5254f4f093fd7c0
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-27 13:56:56 -0300

    i18n + json_api Adapter com active model serializer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,12 +7,6 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
-  def as_json(options = {})
-    hash = super(options)
-    hash[:birthdate] = I18n.l(self.birthdate) unless self.birthdate.blank?
-    hash
-  end
-
   # def birthdate_br
   #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,10 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    hash = super(*args)
+    # pt-BR => hash[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
+    hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    hash
+  end
+end
